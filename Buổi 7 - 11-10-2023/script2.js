287838cc4aeb711067a7b50750b7f53d860e61c1
longlamduc <longlamduc@gmail.com>
2023-10-25 20:36:12 +0700

    Update code lesson 7

---
+++ Buổi 7 - 11-10-2023/script2.js
@@ -0,0 +1,8 @@
+let length = parseInt(prompt("Please input length of rectangle:"));
+let width = parseInt(prompt("Please input width of rectangle: "));
+
+let perimeter = (length + width) * 2;
+let area = length * width;
+
+alert(perimeter);
+alert(area);
